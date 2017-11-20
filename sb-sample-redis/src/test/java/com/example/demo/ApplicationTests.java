1e795906ebc5ff805f4fe3c57f1941ae91eb3493
zhangzhengtao <zhangzhengtao@zhangzhtaosmini.lan>
2017-11-20 13:57:27 +0800

    update

--- sb-sample-redis/src/test/java/com/example/demo/ApplicationTests.java
+++ sb-sample-redis/src/test/java/com/example/demo/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.demo.domain.User;
-
-import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
