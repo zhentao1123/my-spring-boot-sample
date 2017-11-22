c2d7476f0945061f22553176c3a5fe730e842c25
zhangzhengtao <zhangzhengtao@zhangzhtaosmini.lan>
2017-11-22 10:10:52 +0800

    modify

--- sb-sample-jdbctemplate-2/src/test/java/com/example/demo/ApplicationTests.java
+++ sb-sample-jdbctemplate-2/src/test/java/com/example/demo/ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,19 +26,19 @@ public class ApplicationTests {
 
 	@Before
 	public void setUp() {
-		jdbcTemplate1.update("DELETE  FROM  USER ");
-		jdbcTemplate2.update("DELETE  FROM  USER ");
+		jdbcTemplate1.update("delete from user");
+		jdbcTemplate2.update("delete from user");
 	}
 
 	@Test
 	public void test() throws Exception {
 
 		// 往第一个数据源中插入两条数据
-		jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
-		jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 2, "bbb", 30);
+		jdbcTemplate1.update("insert into user(id, name, age, create_time) values(?, ?, ?, ?)", 1, "aaa", 20, new Date());
+		jdbcTemplate1.update("insert into user(id, name, age, create_time) values(?, ?, ?, ?)", 2, "bbb", 30, new Date());
 
 		// 往第二个数据源中插入一条数据，若插入的是第一个数据源，则会主键冲突报错
-		jdbcTemplate2.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
+		jdbcTemplate2.update("insert into user(id, name, age, create_time) values(?, ?, ?, ?)", 1, "aaa", 20, new Date());
 
 		// 查一下第一个数据源中是否有两条数据，验证插入是否成功
 		Assert.assertEquals("2", jdbcTemplate1.queryForObject("select count(1) from user", String.class));
