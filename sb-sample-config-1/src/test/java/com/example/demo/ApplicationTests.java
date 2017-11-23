2e4163c61cae25482cc5ebc280fa627e738210e6
zhangzhengtao <zhangzhengtao@zhangzhtaosmini.lan>
2017-11-23 14:18:32 +0800

    create

---
+++ sb-sample-config-1/src/test/java/com/example/demo/ApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(properties = {""})
+@ActiveProfiles("test")
+public class ApplicationTests {
+
+	@Value("dev")
+	String test;
+	
+	@Test
+	public void contextLoads() {
+		Assert.assertEquals("dev", test);
+	}
+
+}
