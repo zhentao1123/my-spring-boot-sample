83f4124a1cb2a7e3efe6f26f97396d1c88242a2f
zhangzhengtao <zhangzhengtao@zhangzhtaosmini.lan>
2017-11-23 15:07:58 +0800

    update

--- sb-sample-jdbctemplate-2/src/main/java/com/example/demo/DataSourceConfig.java
+++ sb-sample-jdbctemplate-2/src/main/java/com/example/demo/DataSourceConfig.java
@@ -9,46 +9,60 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
 public class DataSourceConfig {
 	
-	@Bean(name = "primaryDataSource")
-    @Qualifier("primaryDataSource")
-    @ConfigurationProperties(prefix="spring.datasource.primary")
-    public DataSource primaryDataSource() {
-		/**
-		 1）DataSourceBuilder方式
-			DataSourceBuilder按如下源码中的优先级选择连接池，自动依据存在的包按优先级选取
-			private static final String[] DATA_SOURCE_TYPE_NAMES = new String[] {
-				"org.apache.tomcat.jdbc.pool.DataSource",
-				"com.zaxxer.hikari.HikariDataSource",
-				"org.apache.commons.dbcp.BasicDataSource", // deprecated
-				"org.apache.commons.dbcp2.BasicDataSource" };
-		 2）自指定方式
-		 	如果需要指定连接池实现可以在该处创建并设置指定的连接池包中的DataSource类，并按设置特定属性
-		 */
-        return DataSourceBuilder.create().build();
-    }
+	//--DataSourceTransactionManager-----------------------------------------
+	/**
+	 * 1）事务处理需要注意TransactionManager和JdbcTemplate要对应，即同DataSource
+	 * 2）如果是基于读写分离的目的，需要注意：
+	 * a.读操作不开启事务
+	 * b.读写操作区分调用对应的JdbcTemplate读对应的库（如读取最新生成的记录ID这种可以直接读写库）
+	 * c.务必不要在读库上做写操作
+	 */
+//	@Bean(name = "primaryDataSourceTransactionManager")
+//    @Qualifier("primaryDataSourceTransactionManager")
+//	public DataSourceTransactionManager primaryDataSourceTransactionManager(@Qualifier("primaryDataSource") DataSource dataSource){
+//		DataSourceTransactionManager dtm = new DataSourceTransactionManager();
+//		dtm.setDataSource(dataSource);
+//		return dtm;
+//	}
+	
+	@Bean(name = "secondaryDataSourceTransactionManager")
+    @Qualifier("secondaryDataSourceTransactionManager")
+	@Primary //写库
+	public DataSourceTransactionManager secondaryDataSourceTransactionManager(@Qualifier("secondaryDataSource") DataSource dataSource){
+		DataSourceTransactionManager dtm = new DataSourceTransactionManager();
+		dtm.setDataSource(dataSource);
+		return dtm;
+	}
 
-    @Bean(name = "secondaryDataSource")
-    @Qualifier("secondaryDataSource")
-    @Primary
-    @ConfigurationProperties(prefix="spring.datasource.secondary")
-    public DataSource secondaryDataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
+    //--JdbcTemplate-----------------------------------------
     @Bean(name = "primaryJdbcTemplate")
-    public JdbcTemplate primaryJdbcTemplate(
-            @Qualifier("primaryDataSource") DataSource dataSource) {
+    public JdbcTemplate primaryJdbcTemplate(@Qualifier("primaryDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
 
     @Bean(name = "secondaryJdbcTemplate")
-    public JdbcTemplate secondaryJdbcTemplate(
-            @Qualifier("secondaryDataSource") DataSource dataSource) {
+    public JdbcTemplate secondaryJdbcTemplate(@Qualifier("secondaryDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
 	
+    //--DataSource-----------------------------------------
+  	@Bean(name = "primaryDataSource")
+	@Qualifier("primaryDataSource")
+	@ConfigurationProperties(prefix="spring.datasource.primary")
+	public DataSource primaryDataSource() {
+	    return DataSourceBuilder.create().build();
+	}
+	
+	@Bean(name = "secondaryDataSource")
+	@Qualifier("secondaryDataSource")
+	@Primary //写库
+	@ConfigurationProperties(prefix="spring.datasource.secondary")
+	public DataSource secondaryDataSource() {
+	    return DataSourceBuilder.create().build();
+	}
 }
