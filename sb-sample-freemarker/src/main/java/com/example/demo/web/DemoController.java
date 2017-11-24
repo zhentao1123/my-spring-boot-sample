14f391f2e3e4d7237d80261a8be45668eaf089d5
zhangzhengtao <zhangzhengtao@zhangzhtaosmini.lan>
2017-11-24 14:50:24 +0800

    modify

--- sb-sample-freemarker/src/main/java/com/example/demo/web/DemoController.java
+++ sb-sample-freemarker/src/main/java/com/example/demo/web/DemoController.java
@@ -1,11 +1,8 @@
 package com.example.demo.web;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
