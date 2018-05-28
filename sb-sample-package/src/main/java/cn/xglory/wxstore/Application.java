f74325ac400fc71702c952fb311d5cd541af5db0
Bob Zhang <zzt1981@126.com>
2018-05-28 18:25:51 +0800

    update

--- sb-sample-package/src/main/java/cn/xglory/wxstore/Application.java
+++ sb-sample-package/src/main/java/cn/xglory/wxstore/Application.java
@@ -1,5 +1,8 @@
 package cn.xglory.wxstore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +11,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		/*
+		SpringApplication app = new SpringApplication(Application.class);
+		Map<String, Object> defProperties = new HashMap<>();
+		defProperties.put("spring.profiles.default", "dev");
+		app.setDefaultProperties(defProperties);
+		app.run(args);
+		*/
 	}
 }
