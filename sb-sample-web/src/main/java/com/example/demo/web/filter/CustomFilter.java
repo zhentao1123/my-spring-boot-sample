713c1b4e84ecdb6b1b6a5c970b62c0f5d084727d
Bob Zhang <zzt1981@126.com>
2018-06-07 10:48:16 +0800

    update

--- sb-sample-web/src/main/java/com/example/demo/web/filter/CustomFilter.java
+++ sb-sample-web/src/main/java/com/example/demo/web/filter/CustomFilter.java
@@ -12,27 +12,43 @@ import javax.servlet.annotation.WebFilter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
+/**
+ * 过滤器添加方式一
+ */
+@Configuration //必须
 @WebFilter(urlPatterns="/*")
+@Order(value = 1)
 public class CustomFilter implements Filter{
 
 	private static Logger logger = LoggerFactory.getLogger(CustomFilter.class);
 	
-	@Override
-	public void destroy() {
-		logger.debug("[filter destory]");
-	}
-
+	/**
+	 * 请求时候调用
+	 */
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		logger.debug("[filter do]");
 		chain.doFilter(request, response);
 	}
-
+	
+	/**
+	 * 应用启动时候调用
+	 */
 	@Override
 	public void init(FilterConfig config) throws ServletException {
 		logger.debug("[filter init]");
+	}
+	
+	/**
+	 * 应用销毁时候调用
+	 */
+	@Override
+	public void destroy() {
+		logger.debug("[filter destory]");
 	}
 
 }
